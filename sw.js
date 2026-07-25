07706f444cfe98c12fdd576c02e6d4257bdfa299
Xdrjian <140455752+Xdrjian@users.noreply.github.com>
2026-07-25 13:09:42 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Service Worker - 基础离线支持
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            return caches.match(event.request);
-        })
-    );
-});
